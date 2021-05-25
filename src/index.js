2c98e1c3d25174821ea393e9a6ecb595ff655e8d
Felipe Silva <felipe.dacs@gmail.com>
2021-05-25 19:40:40 -0300

    :tv: add aula 2

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// yarn babel src/index.js --out-file dist/bundle.js
+function App(){
+  return <h2>Hello</h2>
+}
+/*
+const user = {
+  name: 'Diego'
+}
+
+// yarn babel src/index.js --out-file dist/bundle.js
+console.log(user.address?.street)
+*/
