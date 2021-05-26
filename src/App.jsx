51a94e6de8577297f5e877ff57b0a8098ee97f13
Felipe Silva <felipe.dacs@gmail.com>
2021-05-25 23:17:27 -0300

    :tv: add aula 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { RepositoryList } from './components/RepositoryList'
-import { Contador } from './components/Contador'
+import './styles/global.scss'
 
 
 export function App(){
   return (
     <>
     <RepositoryList />
-    <Contador />
     </>
   )
 }
